8393f00e76769390794b72b71e62e9644985c92c
JanMalch <25508038+JanMalch@users.noreply.github.com>
2026-02-08 11:18:19 +0100

    feat: prepare basic main screen

---
+++ app/src/main/java/io/github/janmalch/multitimer/core/DurationFormat.kt
@@ -0,0 +1,40 @@
+package io.github.janmalch.multitimer.core
+
+import androidx.annotation.VisibleForTesting
+import kotlin.time.Duration
+
+@JvmInline
+value class DurationFormat(private val format: String) {
+    init {
+        require(format.isNotBlank()) {
+            "Duration format cannot be blank, but got \"$format\"."
+        }
+        for (ch in format) {
+            if (!ch.isDigit()) continue
+            val digit = ch.digitToInt(10)
+            require(digit in 1..5) {
+                "Duration format may only include the digits 1, 2, and 3, but got \"$format\"."
+            }
+        }
+        require("1" in format) {
+            "Duration format must include '1' as hours placeholder, but got \"$format\"."
+        }
+        require("23" in format) {
+            "Duration format must include '23' as minutes placeholder, but got \"$format\"."
+        }
+        if ('4' in format || '5' in format) {
+            require("45" in format) {
+                "Duration format has malformed seconds placeholder. Should be '45' but got \"$format\"."
+            }
+        }
+    }
+
+    fun format(duration: Duration): String = format
+        .replace("1", duration.inWholeHours.toString(10))
+        .replace("23", (duration.inWholeMinutes % 60).toString(10).padStart(2, '0'))
+        .replace("45", (duration.inWholeSeconds % 60).toString(10).padStart(2, '0'))
+
+    companion object {
+        fun isValid(format: String): Boolean = runCatching { DurationFormat(format) }.isSuccess
+    }
+}
--- app/src/main/java/io/github/janmalch/multitimer/ui/screens/main/MainScreen.kt
+++ app/src/main/java/io/github/janmalch/multitimer/ui/screens/main/MainScreen.kt
@@ -1,15 +1,48 @@
 package io.github.janmalch.multitimer.ui.screens.main
 
+import androidx.annotation.VisibleForTesting
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.toColorInt
+import io.github.janmalch.multitimer.core.DurationFormat
+import io.github.janmalch.multitimer.core.Event
+import io.github.janmalch.multitimer.models.Timer
 import io.github.janmalch.multitimer.ui.theme.MultiTimerTheme
 import kotlinx.coroutines.delay
 import kotlin.time.Clock
@@ -20,7 +53,7 @@ import kotlin.time.Instant
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Timer(
+    TimerText(
         base = Duration.ZERO,
         since = remember { Clock.System.now() },
         isRunning = true,
@@ -28,32 +61,175 @@ fun MainScreen(modifier: Modifier = Modifier) {
 }
 
 @Composable
-private fun Timer(
+private fun MainScreen(
+    mostRecentEvent: Event?,
+    timers: List<Timer>,
+    onStopClick: () -> Unit,
+    onTimerClick: (Timer) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Button(
+            onClick = onStopClick,
+            enabled = mostRecentEvent?.timerName != null,
+        ) {
+            Text(
+                text = "Stop",
+            )
+        }
+        HorizontalDivider()
+        TimersColumn(
+            mostRecentEvent = mostRecentEvent,
+            timers = timers,
+            onTimerClick = onTimerClick,
+            modifier = Modifier.padding(vertical = 24.dp),
+        )
+    }
+}
+
+// Source - https://stackoverflow.com/a/60479489
+// Posted by burkinafaso3741, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-08, License - CC BY-SA 4.0
+private fun Color.Companion.fromTimer(timer: Timer): Color =
+    try {
+        Color(("#" + timer.color.trimStart('#')).toColorInt())
+    } catch (_: Exception) {
+        Color.Gray
+    }
+
+
+@Composable
+private fun TimersColumn(
+    mostRecentEvent: Event?,
+    timers: List<Timer>,
+    onTimerClick: (Timer) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        timers.forEachIndexed { i, timer ->
+            val shape = RoundedCornerShape(
+                topStart = if (i == 0) 16.dp else 4.dp,
+                topEnd = if (i == 0) 16.dp else 4.dp,
+                bottomEnd = if (i == timers.lastIndex) 16.dp else 4.dp,
+                bottomStart = if (i == timers.lastIndex) 16.dp else 4.dp,
+            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(shape)
+                    .clickable(onClick = { onTimerClick(timer) })
+                    .background(MaterialTheme.colorScheme.surface, shape)
+                    .padding(vertical = 12.dp, horizontal = 16.dp),
+            ) {
+                val isRunning = mostRecentEvent?.timerName == timer.name
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(32.dp)
+                            .background(Color.fromTimer(timer), RoundedCornerShape(4.dp))
+                    )
+                    Text(
+                        text = timer.name,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        fontWeight = FontWeight.Bold.takeIf { isRunning },
+                    )
+                }
+                TimerText(
+                    base = Duration.ZERO,
+                    since = remember { Clock.System.now() },
+                    isRunning = isRunning,
+                )
+            }
+            Spacer(Modifier.height(1.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TimersColumnPreview() {
+    MultiTimerTheme {
+        Surface(color = MaterialTheme.colorScheme.surfaceDim) {
+            TimersColumn(
+                mostRecentEvent = null,
+                timers = listOf(
+                    Timer.newBuilder().setName("A").setColor("#FF0000").build(),
+                    Timer.newBuilder().setName("B").setColor("#00FF00").build(),
+                    Timer.newBuilder().setName("C").setColor("#0000FF").build(),
+                ),
+                onTimerClick = {},
+                modifier = Modifier.padding(24.dp),
+            )
+        }
+    }
+}
+
+private fun durationSaver(): Saver<Duration, Long> = Saver(
+    save = { it.inWholeMilliseconds },
+    restore = { it.milliseconds }
+)
+
+@Composable
+private fun TimerText(
     base: Duration,
     since: Instant,
     isRunning: Boolean,
     clock: Clock = remember { Clock.System },
+    format: DurationFormat = remember { DurationFormat("1:23:45") }
 ) {
-    var elapsedMs by rememberSaveable(base) { mutableLongStateOf(base.inWholeMilliseconds) }
+    var elapsed by rememberSaveable(base, stateSaver = durationSaver()) { mutableStateOf(base) }
     LaunchedEffect(isRunning, since, base) {
         if (!isRunning) {
-            elapsedMs = base.inWholeMilliseconds
+            elapsed = base
             return@LaunchedEffect
         }
         while (isRunning) {
-            elapsedMs = ((clock.now() - since) + base).inWholeMilliseconds
-            delay(50)
+            elapsed = ((clock.now() - since) + base)
+            delay(200)
         }
     }
-    Text(elapsedMs.milliseconds.toString())
+    Text(format.format(elapsed))
 }
 
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun MainScreenPreview() {
+    var mostRecentEvent by remember { mutableStateOf<Event?>(null) }
+    MultiTimerTheme {
+        MainScreen(
+            mostRecentEvent = mostRecentEvent,
+            timers = listOf(
+                Timer.newBuilder().setName("A").setColor("#FF0000").build(),
+                Timer.newBuilder().setName("B").setColor("#00FF00").build(),
+                Timer.newBuilder().setName("C").setColor("#0000FF").build(),
+            ),
+            onStopClick = {
+                mostRecentEvent = Event(timerName = null, timestamp = Clock.System.now())
+            },
+            onTimerClick = { timer ->
+                mostRecentEvent = Event(
+                    timerName = timer.name.takeUnless { it == mostRecentEvent?.timerName },
+                    timestamp = Clock.System.now(),
+                )
+            },
+            modifier = Modifier.padding(24.dp),
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-private fun TimerFromZeroRunningPreview() {
+private fun TimerTextFromZeroRunningPreview() {
     MultiTimerTheme {
-        Timer(
+        TimerText(
             base = Duration.ZERO,
             since = remember { Clock.System.now() },
             isRunning = false,
@@ -63,9 +239,9 @@ private fun TimerFromZeroRunningPreview() {
 
 @Preview(showBackground = true)
 @Composable
-private fun TimerFromZeroNotRunningPreview() {
+private fun TimerTextFromZeroNotRunningPreview() {
     MultiTimerTheme {
-        Timer(
+        TimerText(
             base = Duration.ZERO,
             since = remember { Clock.System.now() },
             isRunning = true,
@@ -75,9 +251,9 @@ private fun TimerFromZeroNotRunningPreview() {
 
 @Preview(showBackground = true)
 @Composable
-private fun TimerFromOneHourRunningPreview() {
+private fun TimerTextFromOneHourRunningPreview() {
     MultiTimerTheme {
-        Timer(
+        TimerText(
             base = 1.hours,
             since = remember { Clock.System.now() },
             isRunning = true,
@@ -87,9 +263,9 @@ private fun TimerFromOneHourRunningPreview() {
 
 @Preview(showBackground = true)
 @Composable
-private fun TimerFromOneHourNotRunningPreview() {
+private fun TimerTextFromOneHourNotRunningPreview() {
     MultiTimerTheme {
-        Timer(
+        TimerText(
             base = 1.hours,
             since = remember { Clock.System.now() },
             isRunning = false,

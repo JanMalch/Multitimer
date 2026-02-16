12ff304265e7d8f2dac77bf23800967436cad6e9
JanMalch <25508038+JanMalch@users.noreply.github.com>
2026-02-16 20:43:22 +0100

    feat: add share button

--- app/src/main/java/io/github/janmalch/multitimer/ui/screens/main/MainScreen.kt
+++ app/src/main/java/io/github/janmalch/multitimer/ui/screens/main/MainScreen.kt
@@ -1,5 +1,6 @@
 package io.github.janmalch.multitimer.ui.screens.main
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,8 +14,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -33,6 +36,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -45,6 +49,7 @@ import io.github.janmalch.multitimer.core.DurationFormat
 import io.github.janmalch.multitimer.core.Event
 import io.github.janmalch.multitimer.core.TodaysTimer
 import io.github.janmalch.multitimer.models.Timer
+import io.github.janmalch.multitimer.ui.components.CollectAsEvent
 import io.github.janmalch.multitimer.ui.components.TimerColorBox
 import io.github.janmalch.multitimer.ui.theme.MultiTimerTheme
 import kotlinx.coroutines.delay
@@ -64,10 +69,20 @@ fun MainScreen(
 ) {
     val mostRecentEvent by viewModel.mostRecentEvent.collectAsStateWithLifecycle()
     val timers by viewModel.timers.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    CollectAsEvent(viewModel.onShareIntent) {
+        try {
+            context.startActivity(it)
+        } catch (e: Exception) {
+            Log.e("MainScreen", "Failed to handle share intent.", e)
+        }
+    }
+
     MainScreen(
         mostRecentEvent = mostRecentEvent,
         timers = timers,
         goToConfigScreen = goToConfigScreen,
+        onShareReport = { viewModel.shareReport() },
         onStopClick = { viewModel.addEvent(null) },
         onTimerClick = { viewModel.addEvent(it.name) },
         modifier = modifier,
@@ -80,6 +95,7 @@ private fun MainScreen(
     mostRecentEvent: Event?,
     timers: List<TodaysTimer>,
     goToConfigScreen: () -> Unit,
+    onShareReport: () -> Unit,
     onStopClick: () -> Unit,
     onTimerClick: (Timer) -> Unit,
     modifier: Modifier = Modifier
@@ -99,6 +115,11 @@ private fun MainScreen(
                     }
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onShareReport) {
+                Icon(Icons.Default.Share, contentDescription = "Export report")
+            }
         }
     ) { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
@@ -255,6 +276,7 @@ private fun MainScreenPreview() {
                 mostRecentEvent = Event(timerName = null, timestamp = Clock.System.now())
             },
             goToConfigScreen = {},
+            onShareReport = {},
             onTimerClick = { timer ->
                 mostRecentEvent = Event(
                     timerName = timer.name.takeUnless { it == mostRecentEvent?.timerName },
--- app/src/main/java/io/github/janmalch/multitimer/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/io/github/janmalch/multitimer/ui/screens/main/MainViewModel.kt
@@ -1,31 +1,48 @@
 package io.github.janmalch.multitimer.ui.screens.main
 
+import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.github.janmalch.multitimer.core.EventRepository
+import io.github.janmalch.multitimer.core.Report
 import io.github.janmalch.multitimer.core.TodaysTimersUseCase
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val todaysTimersUseCase: TodaysTimersUseCase,
+    todaysTimersUseCase: TodaysTimersUseCase,
     private val eventRepository: EventRepository,
+    @ApplicationContext private val context: Context,
 ) : ViewModel() {
+
+    private val ioDispatcher: CoroutineContext = Dispatchers.IO
 
     private val _onEventFailed = Channel<Unit>()
     val onEventFailed = _onEventFailed.receiveAsFlow()
+
+    private val _onShareFailed = Channel<Unit>()
+    val onShareFailed = _onShareFailed.receiveAsFlow()
+
+    private val _onShareIntent = Channel<Intent>()
+    val onShareIntent = _onShareIntent.receiveAsFlow()
 
     val timers = todaysTimersUseCase()
         .stateIn(
@@ -50,6 +67,40 @@ class MainViewModel @Inject constructor(
             } catch (e: Exception) {
                 Log.e("MainViewModel", "Failed to add event.", e)
                 _onEventFailed.send(Unit)
+            }
+        }
+    }
+
+    fun shareReport() {
+        viewModelScope.launch(ioDispatcher) {
+            try {
+                val report = Report(eventRepository.todaysEvents().first())
+                val content = report.toTextReport()
+                val file = context.filesDir.resolve("report.txt").apply {
+                    writeText(content)
+                }
+
+                val uri = FileProvider.getUriForFile(
+                    context,
+                    "io.github.janmalch.multitimer.fileprovider",
+                    file
+                )
+                val intent = Intent.createChooser(
+                    Intent(Intent.ACTION_SEND).apply {
+                        type = "text/plain"
+                        putExtra(Intent.EXTRA_STREAM, uri)
+                        putExtra("EXTRA_FILE_PATH", file)
+                        flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    },
+                    // FIXME: resources
+                    "Export report"
+                )
+                _onShareIntent.send(intent)
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Exception) {
+                Log.e("MainViewModel", "Failed to share report.", e)
+                _onShareFailed.send(Unit)
             }
         }
     }
